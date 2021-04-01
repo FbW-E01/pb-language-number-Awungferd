33162f9fad16e24ccd891b15bf6d59fb74860b6a
Ferdinand <ndimbecha@gmail.com>
2021-04-01 10:05:07 +0200

    Concatenate and Convert

---
+++ stringsNumbers.js
@@ -0,0 +1,15 @@
+// 1) CONCATENATION
+let greetings = "Hi, we are going to number ";
+let streetNumber = 105;
+console.log(greetings.concat(streetNumber));
+console.log(greetings + streetNumber);
+
+//Here concatenation is going on. That is, JS takes the value of the string and combines it with the value of the number to produce a new result.
+//The + adds up the two values to produce a new result
+
+// 2) CONVERTING
+let peterPan = "1005";
+console.log(parseInt(peterPan));
+
+let panPeter = "10.5";
+console.log(parseFloat(panPeter));
